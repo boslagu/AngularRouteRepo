77ddbece1681e929da1dd54c8de91da70193fba2
Kurisuchan <rlagurin.bos@gmail.com>
2017-05-17 16:48:30 +0800

    Very first commit

---
+++ public/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+
+angular.module('myApp', [
+  'myApp.controllers',
+  'myApp.filters',
+  'myApp.services',
+  'myApp.directives'
+]).
+config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/Home', {
+      templateUrl: 'partials/Home',
+      controller: 'HomeCtrl'
+    }).
+    when('/Quiz', {
+      templateUrl: 'partials/Quiz',
+      controller: 'QuizCtrl'
+    }).
+    when('/Developer', {
+      templateUrl: 'partials/Developer',
+      controller: 'DeveloperCtrl'
+    }).
+    otherwise({
+      redirectTo: '/Home'
+    });
+
+  $locationProvider.html5Mode(true);
+});
