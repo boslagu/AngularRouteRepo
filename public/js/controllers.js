3fde48c5578b8f9687cdf18f97534ce1036f0906
MoniqueNoche <mnoche.bos@gmail.com>
2017-05-19 14:53:48 +0800

    Controllers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,30 +1,30 @@
-'use strict';
-
-/* Controllers */
-
-angular.module('myApp.controllers', []).
-  controller('AppCtrl', function ($scope, $http) {
-
-    $http({
-      method: 'GET',
-      url: '/api/name'
-    }).
-    success(function (data, status, headers, config) {
-      $scope.name = data.name;
-    }).
-    error(function (data, status, headers, config) {
-      $scope.name = 'Error!';
-    });
-
-  }).
-  controller('HomeCtrl', function ($scope) {
-    // write Ctrl here
-    //$scope.variablename = "showthis"
-
-  }).
-  controller('QuizCtrl', function ($scope) {
-    // write Ctrl here
-  }).
-  controller('DeveloperCtrl', function ($scope) {
-    // write Ctrl here
-  });
+// 'use strict';
+//
+// /* Controllers */
+//
+// angular.module('myApp.controllers', []).
+//   controller('AppCtrl', function ($scope, $http) {
+//
+//     $http({
+//       method: 'GET',
+//       url: '/api/name'
+//     }).
+//     success(function (data, status, headers, config) {
+//       $scope.name = data.name;
+//     }).
+//     error(function (data, status, headers, config) {
+//       $scope.name = 'Error!';
+//     });
+// 
+//   }).
+//   controller('HomeCtrl', function ($scope) {
+//     // write Ctrl here
+//     //$scope.variablename = "showthis"
+//
+//   }).
+//   controller('QuizCtrl', function ($scope) {
+//     // write Ctrl here
+//   }).
+//   controller('DeveloperCtrl', function ($scope) {
+//     // write Ctrl here
+//   });
---
+++ public/js/controllers/HomeController.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('AppCtrl', function ($scope, $http) {
+
+    $http({
+      method: 'GET',
+      url: '/api/name'
+    }).
+    success(function (data, status, headers, config) {
+      $scope.name = data.name;
+    }).
+    error(function (data, status, headers, config) {
+      $scope.name = 'Error!';
+    });
+
+  }).
+  controller('HomeCtrl', function ($scope) {
+    // write Ctrl here
+    //$scope.variablename = "showthis"
+
+
+  });
